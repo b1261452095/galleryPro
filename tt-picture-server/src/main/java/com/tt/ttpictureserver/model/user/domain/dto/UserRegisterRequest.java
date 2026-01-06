0e3ca815aa6f0c06f5a3dd932835b9d9afcf272a
NoBug <>
2026-01-06 16:01:38 +0800

    注册功能提交

---
+++ tt-picture-server/src/main/java/com/tt/ttpictureserver/model/user/domain/dto/UserRegisterRequest.java
@@ -0,0 +1,10 @@
+package com.tt.ttpictureserver.model.user.domain.dto;
+
+/**
+ * @project tt-picture-server
+ * @description: 用户注册请求参数
+ * @author NoBug
+ * @date 2026-01-06 15:28
+ * @version 1.0
+ */public class UserRegisterRequest {
+}
