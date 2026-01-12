d84fab308fb3debd0268b6dc1690e3c4f49daa78
LMNOP <1>
2026-01-13 00:02:09 +0800

    提交代码

---
+++ tt-picture-server/src/main/java/com/tt/ttpictureserver/model/picture/domain/dto/PictureQueryRequest.java
@@ -0,0 +1,78 @@
+package com.tt.ttpictureserver.model.picture.domain.dto;
+
+import com.tt.ttpictureserver.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class PictureQueryRequest extends PageRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 图片 url
+     */
+    private String url;
+
+    /**
+     * 图片名称
+     */
+    private String name;
+
+    /**
+     * 简介
+     */
+    private String introduction;
+
+    /**
+     * 分类
+     */
+    private String category;
+
+    /**
+     * 标签（JSON 数组）
+     */
+    private List<String> tags;
+
+    /**
+     * 图片体积
+     */
+    private Long picSize;
+
+    /**
+     * 图片宽度
+     */
+    private Integer picWidth;
+
+    /**
+     * 图片高度
+     */
+    private Integer picHeight;
+
+    /**
+     * 图片宽高比例
+     */
+    private Double picScale;
+
+    /**
+     * 图片格式
+     */
+    private String picFormat;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+
+}
